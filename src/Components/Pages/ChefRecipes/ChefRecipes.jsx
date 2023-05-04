0f0257dec25735609edd27de2b874e7f41e36006
Al Mamun <almamunshisir770@gmail.com>
2023-05-04 18:48:27 +0600

    chnages some style in chefrecipe component

--- src/Components/Pages/ChefRecipes/ChefRecipes.jsx
+++ src/Components/Pages/ChefRecipes/ChefRecipes.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
-import bgImage from "../../../assets/foodPic/brooke-lark-08bOYnH_r_E-unsplash.jpg"
+import bgImage from "../../../assets/foodPic/brooke-lark-4J059aGa5s4-unsplash.jpg"
 import RecipesCard from '../RecipesCard/RecipesCard';
-import { FaCuttlefish, FaHeart } from 'react-icons/fa';
+import {  FaHamburger, FaHeart } from 'react-icons/fa';
 const ChefRecipes = () => {
 const recipes = useLoaderData();
 const {id} = useParams()
@@ -14,27 +14,28 @@ useEffect(()=>{
 },[]) 
 return (
  <div>
-    <div className="bg-cover bg-center" style={{ backgroundImage: `url(${bgImage})`, backgroundColor: "rgba(255, 255, 255, 0.8)" }}>
+    <div className="bg-cover bg-center hero " style={{ backgroundImage: `url(${bgImage})`}}>
+    <div className="hero-overlay bg-opacity-50"></div>
   <div className="container mx-auto px-4 py-32 text-black">
     <div className="flex flex-col md:flex-row items-center justify-center gap-6 h-5/6">
       <div className="md:w-1/4 mb-4 md:mb-0">
         <img className="rounded-full md:h-80 md:w-80 w-64 h-64 border-4 border-white shadow-lg" src={chefData.chef_picture} alt="Chef image" />
       </div>
       <div className="md:w-3/4 md:pl-10 md:text-left text-center">
-        <h1 className="md:text-5xl text-4xl font-bold mb-5 text-green-500 title">{chefData.chef_name}</h1>
-        <p className="md:text-2xl text-xl text-gray-700 font-bold mb-6">{chefData.chef_bio}</p>
+        <h1 className="md:text-5xl text-4xl font-bold mb-5 text-green-400 title">{chefData.chef_name}</h1>
+        <p className="md:text-2xl text-xl text-white font-bold mb-6">{chefData.chef_bio}</p>
         <div className="flex flex-wrap justify-between items-center mb-6">
           <div className="w-full sm:w-1/2 md:w-1/3 mb-4 md:mb-0">
-            <p className="md:text-3xl text-2xl text-green-500 title font-bold">Experience</p>
-            <p className="md:text-2xl text-xl text-gray-700 font-semibold">{chefData.years_of_experience} Years</p>
+            <p className="md:text-3xl text-2xl text-green-400 title font-bold">Experience</p>
+            <p className="md:text-2xl text-xl text-white font-semibold">{chefData.years_of_experience} Years</p>
           </div>
           <div className="w-full sm:w-1/2 md:w-1/3 mb-4 md:mb-0">
-            <p className="md:text-3xl text-2xl text-green-500 title font-bold">Likes</p>
-            <p className="md:text-2xl text-xl text-gray-700 font-semibold inline-flex items-center gap-1">{chefData.likes}<span><FaHeart className='text-red-600 text-center'></FaHeart></span></p>
+            <p className="md:text-3xl text-2xl text-green-400 title font-bold">Likes</p>
+            <p className="md:text-2xl text-xl text-white font-semibold inline-flex items-center gap-1">{chefData.likes}<span><FaHeart className='text-red-600 text-center'></FaHeart></span></p>
           </div>
           <div className="w-full sm:w-1/2 md:w-1/3 mb-4 md:mb-0">
-            <p className="md:text-3xl text-2xl text-green-500 title font-bold">Recipes</p>
-            <p className="md:text-2xl text-xl text-gray-700 font-semibold inline-flex items-center">{chefData.number_of_recipes}<span><FaCuttlefish className='text-red-600 text-center'></FaCuttlefish> </span></p>
+            <p className="md:text-3xl text-2xl text-green-400 title font-bold">Recipes</p>
+            <p className="md:text-2xl text-xl text-white font-semibold inline-flex items-center gap-1">{chefData.number_of_recipes}<span><FaHamburger className='text-yellow-600 text-center'></FaHamburger> </span></p>
           </div>
         </div>
       </div>
--- src/privateRoute/PrivateRoute.jsx
+++ src/privateRoute/PrivateRoute.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../Provider/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
+import { ColorRing } from 'react-loader-spinner'
 
 const PrivateRoute = ({children}) => {
     const { user, loading} = useContext(AuthContext)
@@ -8,7 +9,19 @@ const PrivateRoute = ({children}) => {
     // console.log('user in private route',user);
     const from = location.state?.from?.pathname || "/";
     if (loading) {
-        return <div className='max-w-sm mx-auto '><div className="radial-progress text-primary " style={{ "--value": 100 }}>100%</div></div>
+        return <div className='mx-auto text-center h-screen flex items-center justify-center'>
+      <div>
+       <ColorRing
+        visible={true}
+        height="150"
+        width="150"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        colors={['#b8c480', '#B2A3B5', '#F4442E', '#51E5FF', '#429EA6']}
+      />
+      </div>
+        </div>
 
     }
     if (user) {
@@ -18,3 +31,21 @@ const PrivateRoute = ({children}) => {
 };
 
 export default PrivateRoute;
+
+
+
+
+
+
+
+
+
+{/* <ColorRing
+  visible={true}
+  height="80"
+  width="80"
+  ariaLabel="blocks-loading"
+  wrapperStyle={{}}
+  wrapperClass="blocks-wrapper"
+  colors={['#b8c480', '#B2A3B5', '#F4442E', '#51E5FF', '#429EA6']}
+/> */}
